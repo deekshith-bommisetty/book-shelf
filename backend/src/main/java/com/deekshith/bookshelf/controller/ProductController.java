0926804516ea1c443661aeb9bcbbc81d7a43d340
deekshith-bommisetty <deekshith.bommisetty@gmail.com>
2022-04-20 00:15:46 -0400

    Products module adapter pattern

--- backend/src/main/java/com/deekshith/bookshelf/controller/ProductController.java
+++ backend/src/main/java/com/deekshith/bookshelf/controller/ProductController.java
@@ -5,11 +5,10 @@ import com.deekshith.bookshelf.model.Product;
 import com.deekshith.bookshelf.model.ProductReview;
 import com.deekshith.bookshelf.payload.request.ProductRequest;
 import com.deekshith.bookshelf.payload.response.MessageResponse;
-import com.deekshith.bookshelf.repository.ProductRepository;
-import com.deekshith.bookshelf.service.ProductService;
-import com.deekshith.bookshelf.service.UserService;
+import com.deekshith.bookshelf.service.ProductServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,17 +20,14 @@ import java.util.List;
 public class ProductController {
 
     @Autowired
-    ProductRepository productRepository;
+    ProductServiceImpl productService;
 
-    @Autowired
-    UserService userService;
-
-    @Autowired
-    ProductService productService;
-
+    // @desc    Create new review
+    // @route   POST /api/products/:id/reviews
+    // @access  Private
     @RequestMapping(value = "/api/products/{id}/review", method = RequestMethod.POST)
     public ResponseEntity<?> createProductReview(@PathVariable("id") String id, @RequestBody ProductReview productReview) {
-        Product product = productService.retrieveProduct(id);
+        Product product = productService.getProduct(id);
         UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if(product != null){
             if(product.getReviews() != null){
@@ -51,7 +47,7 @@ public class ProductController {
                  accumulator = accumulator + currentReview.getRating();
             }
             product.setRating((Double) accumulator/(Integer) product.getReviews().size());
-            return ResponseEntity.ok(productRepository.save(product));
+            return ResponseEntity.ok(productService.saveProduct(product));
         } else {
             return ResponseEntity
                     .badRequest()
@@ -59,7 +55,11 @@ public class ProductController {
         }
     }
 
+    // @desc    Update a product
+    // @route   PUT /api/products/:id
+    // @access  Private/Admin
     @RequestMapping(value = "/api/products/{id}", method = RequestMethod.PUT)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> updateProduct(@PathVariable("id") String id, @RequestBody ProductRequest productRequest) {
         Product product = productService.updateProduct(id, productRequest);
         if(product != null){
@@ -71,10 +71,14 @@ public class ProductController {
         }
     }
 
+    // @desc    Delete a product
+    // @route   DELETE /api/products/:id
+    // @access  Private/Admin
     @RequestMapping(value = "/api/products/{id}", method = RequestMethod.DELETE)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> deleteProduct(@PathVariable("id") String id) {
 
-        Product product = productService.retrieveProduct(id);
+        Product product = productService.getProduct(id);
         if(product != null){
             productService.deleteProduct(id);
             return ResponseEntity.ok(new MessageResponse("Product has benn deleted"));
@@ -85,10 +89,13 @@ public class ProductController {
         }
     }
 
+    // @desc    Fetch all products
+    // @route   GET /api/products
+    // @access  Public
     @RequestMapping(value = "/api/products", method = RequestMethod.GET)
     public ResponseEntity<?> getAllProducts() {
         // Check for admin role later
-        List<Product> productList = productService.retrieveAllProducts();
+        List<Product> productList = productService.getProducts();
         if(!productList.isEmpty()){
             return ResponseEntity.ok(productList);
         } else {
@@ -97,20 +104,26 @@ public class ProductController {
                     .body(new MessageResponse("Error: Products not found"));
         }
     }
-    // has role admin
-    // take care of adding userReviews
+
+    // @desc    Create a product
+    // @route   POST /api/products
+    // @access  Private/Admin
     @RequestMapping(value = "/api/products", method = RequestMethod.POST)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> createProduct(@Valid @RequestBody ProductRequest productRequest) {
         Product product = new Product(productRequest.getName(), productRequest.getImage(), productRequest.getBrand(), productRequest.getCategory(), productRequest.getDescription(), productRequest.getRating(), productRequest.getNumReviews(), productRequest.getPrice(), productRequest.getCountInStock());
         UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         product.setUser(userDetails.getId().toString());
-        return ResponseEntity.ok(productRepository.save(product));
+        return ResponseEntity.ok(productService.saveProduct(product));
     }
 
+    // @desc    Fetch single product
+    // @route   GET /api/products/:id
+    // @access  Public
     @RequestMapping(value = "/api/products/{id}", method = RequestMethod.GET)
     public ResponseEntity<?> getProductById(@PathVariable("id") String id) {
 
-        Product product = productService.retrieveProduct(id);
+        Product product = productService.getProduct(id);
         if(product != null){
             return ResponseEntity.ok(product);
         } else {
