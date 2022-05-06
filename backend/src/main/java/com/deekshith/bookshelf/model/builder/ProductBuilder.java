12f8a33a5a885330511e9d9cab945e4da14232db
deekshith-bommisetty <deekshith.bommisetty@gmail.com>
2022-05-06 18:20:36 -0400

    builder pattern implemented

---
+++ backend/src/main/java/com/deekshith/bookshelf/model/builder/ProductBuilder.java
@@ -0,0 +1,73 @@
+package com.deekshith.bookshelf.model.builder;
+
+import com.deekshith.bookshelf.model.Product;
+
+public class ProductBuilder {
+
+    private String name;
+
+    private String image;
+
+    private String brand;
+
+    private String category;
+
+    private String description;
+
+    private Double rating;
+
+    private Integer numReviews;
+
+    private Double price;
+
+    private Integer countInStock;
+
+    public ProductBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public ProductBuilder setImage(String image) {
+        this.image = image;
+        return this;
+    }
+
+    public ProductBuilder setBrand(String brand) {
+        this.brand = brand;
+        return this;
+    }
+
+    public ProductBuilder setCategory(String category) {
+        this.category = category;
+        return this;
+    }
+
+    public ProductBuilder setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public ProductBuilder setRating(Double rating) {
+        this.rating = rating;
+        return this;
+    }
+
+    public ProductBuilder setNumReviews(Integer numReviews) {
+        this.numReviews = numReviews;
+        return this;
+    }
+
+    public ProductBuilder setPrice(Double price) {
+        this.price = price;
+        return this;
+    }
+
+    public ProductBuilder setCountInStock(Integer countInStock) {
+        this.countInStock = countInStock;
+        return this;
+    }
+
+    public Product getProducct(){
+        return new Product(name, image, brand, category, description, rating, numReviews, price, countInStock);
+    }
+}
---
+++ backend/src/main/java/com/deekshith/bookshelf/model/builder/UserBuilder.java
@@ -0,0 +1,32 @@
+package com.deekshith.bookshelf.model.builder;
+
+import com.deekshith.bookshelf.model.User;
+
+public class UserBuilder {
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    public UserBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public UserBuilder setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public UserBuilder setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public User getUser(){
+        return new User(name, email, password);
+    }
+
+}
